0f10b85145fa00671eb9cc03384bb1603a6a8501
kritika243 <24kritikasharma24@gmail.com>
2023-06-25 00:05:08 +0530

    basic form inputs inside modal'

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import EditEmployee from './EditEmployee'
+
 const Employee = (props) => {
   return (
     <>
@@ -14,7 +16,8 @@ const Employee = (props) => {
               {props.role || 'role of the employee'}
             </p>
           </div>
-          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Edit</button>
+          <EditEmployee />
+
         </div>
       </div>
     </>
